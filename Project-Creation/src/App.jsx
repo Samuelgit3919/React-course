c397a24228760a55441257fc86336f089a35a17e
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-04-14 09:00:44 +0300

    second lesson

---
+++ Project-Creation/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "./components/Header/Header"
+import "./App.css"
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
